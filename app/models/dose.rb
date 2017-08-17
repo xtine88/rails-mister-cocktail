bf407b17fc86878620aee1987b67fdf49dcd6f43
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-17 12:33:42 +0200

    created doses controller

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :ingredient, uniqueness: { scope: :cocktail}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817094340) do
+ActiveRecord::Schema.define(version: 20170817100903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20170817094340) do
 
   create_table "doses", force: :cascade do |t|
     t.text "description"
-    t.string "cocktail"
-    t.string "references"
+    t.bigint "cocktail_id"
     t.bigint "ingredient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
@@ -37,5 +37,6 @@ ActiveRecord::Schema.define(version: 20170817094340) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
 end
