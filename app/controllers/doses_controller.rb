ef44a95659c1d16ab0e46d4b2f5ca31e54a211e7
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-17 16:46:53 +0200

    modified the create method in Doses Controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,16 @@
 class DosesController < ApplicationController
 
   def new
-    @dose = Dose.new(dose_params)
+    @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
