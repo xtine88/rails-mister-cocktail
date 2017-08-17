546d964f5e0f0f06ba98773aaf9370b09731a288
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-17 16:27:54 +0200

    defined doses controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,11 +18,12 @@ class CocktailsController < ApplicationController
       redirect_to cocktail_path(@cocktail)
     else
       render :new
+    end
   end
 
   private
 
-  def cocktail_params
-    params.require(:cocktail).permit(:name)
-  end
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,29 @@
 class DosesController < ApplicationController
 
   def new
+    @dose = Dose.new(dose_params)
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
   end
 
   def destroy
+    dose = Dose.find(params[:id])
+    cocktail = dose.cocktail
+    dose.destroy
+    redirect_to cocktail_path(cocktail)
   end
+
+  private
+    def dose_params
+      params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+    end
 end
+
+# redirects to the created dose (FAILED - 2)
